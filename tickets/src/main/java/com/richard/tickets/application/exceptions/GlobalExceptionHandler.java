415398c25ab804520ad708f1961d0163ae5f92a0
Ricardo Viana <richardluizv@gmail.com>
2025-05-22 02:32:08 -0300

    create ExceptionHandler

---
+++ tickets/src/main/java/com/richard/tickets/application/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.richard.tickets.application.exceptions;
+
+import com.richard.tickets.application.util.ErrorResponse;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(UserNotFoundException ex) {
+        log.error("Caught UserNotFoundException: ", ex);
+        ErrorResponse error = new ErrorResponse();
+        error.setError("User not found");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException ex) {
+        log.error("Caught MethodArgumentNotValidException: ", ex);
+        ErrorResponse error = new ErrorResponse();
+
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        String errorMessage = fieldErrors.stream().findFirst().map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .orElse("Validation error occurred");
+
+        error.setError(errorMessage);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        log.error("Caught ConstraintViolationException: ", ex);
+        ErrorResponse error = new ErrorResponse();
+
+        String errorMessage = ex.getConstraintViolations()
+                .stream()
+                .findFirst()
+                .map(violation ->
+                        violation.getPropertyPath() + ": " + violation.getMessage()
+                ).orElse("Constraint violation occurred");
+
+        error.setError(errorMessage);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("Caught Exception: ", ex);
+        ErrorResponse error = new ErrorResponse();
+        error.setError("An unknow error occurred.");
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
